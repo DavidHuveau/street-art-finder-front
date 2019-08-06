b348040c490c8904b91694926b7f1e666f787492
David Huveau <d.huveau@gmail.com>
2019-08-06 11:59:25 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import { Button, Alert } from "react-bootstrap";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button variant="primary">test</Button>
+        <Alert variant="primary">This is an alert!</Alert>
       </header>
     </div>
   );
-}
+};
 
 export default App;
