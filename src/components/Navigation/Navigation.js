bfdcf352102fd214ec4fd4206af20147499f5fc7
David Huveau <d.huveau@gmail.com>
2019-08-09 15:10:23 +0200

    design Navigation component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,7 +1,25 @@
 import React from "react";
+import "./Navigation.css";
+import { Nav } from "react-bootstrap";
 
 const Navigation = () => {
-  return <div style={{ border: "solid 1px blue" }}>Navigation</div>;
+  return (
+    <div id="Nav-wrapper">
+      <Nav
+        className="justify-content-center"
+        variant="pills"
+        defaultActiveKey="/home"
+        onSelect={selectedKey => alert(`selected ${selectedKey}`)}
+      >
+        <Nav.Item bsPrefix="Nav-item">
+          <Nav.Link href="/home">Explore</Nav.Link>
+        </Nav.Item>
+        <Nav.Item bsPrefix="Nav-item">
+          <Nav.Link eventKey="suggest">Suggest</Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </div>
+  );
 };
 
 export default Navigation;
