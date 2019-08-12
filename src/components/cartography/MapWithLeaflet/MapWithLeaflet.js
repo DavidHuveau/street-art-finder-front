64705ed7767140128996af0a68d76718228a7fb1
David Huveau <d.huveau@gmail.com>
2019-08-12 15:05:31 +0200

    clean FilmingLocation component

--- src/components/cartography/MapWithLeaflet/MapWithLeaflet.js
+++ src/components/cartography/MapWithLeaflet/MapWithLeaflet.js
@@ -7,22 +7,12 @@ import FilmingLocation from "../FilmingLocation/FilmingLocation";
 import MarkerClusterGroup from "../MarkerClusterGroup/MarkerClusterGroup";
 import dataWithGeo from "../../../constants/data-with-geo";
 
-const GEO_SAN_FRANSISCO = [37.77492, -122.41941];
+const START_POSITION = [37.77492, -122.41941];
 const ZOOM = 13;
-
-const handleMap = event => {
-  console.log("handleMap: ", event.latlng);
-};
 
 export default () => {
   return (
-    <Map
-      className="MyMap"
-      center={GEO_SAN_FRANSISCO}
-      zoom={ZOOM}
-      onClick={handleMap}
-      maxZoom={20}
-    >
+    <Map className="MyMap" center={START_POSITION} zoom={ZOOM} maxZoom={20}>
       <TileLayer
         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -33,7 +23,7 @@ export default () => {
         ))}
       </MarkerClusterGroup>
       <Circle
-        center={GEO_SAN_FRANSISCO}
+        center={START_POSITION}
         fillColor="red"
         bubblingMouseEvents={false}
         radius={200}
