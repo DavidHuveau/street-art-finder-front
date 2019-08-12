712ddd9f0c121d194d7fc271532d09d87abecd0f
David Huveau <d.huveau@gmail.com>
2019-08-12 11:54:25 +0200

    adapt css style

--- src/containers/ArtworksMap/ArtworksMap.js
+++ src/containers/ArtworksMap/ArtworksMap.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import MapWithLeaflet from "../../components/cartography/MapWithLeaflet/MapWithLeaflet";
 
 class ArtworksMap extends Component {
   render() {
     return (
-      <div style={ {border: "solid 1px blue"} }>
-        {/* ArtworksMap */}
+      // <div style={ {border: "solid 1px blue"} }>
+      <div>
         <MapWithLeaflet />
       </div>
-    )
+    );
   }
 }
 
