bdd1f111643bbe2e104d6a9ea90b189d8a2c8db3
David Huveau <d.huveau@gmail.com>
2019-08-14 09:32:56 +0200

    build ArtworkInfosPane component

--- src/components/ArtworkInfosPane/ArtworkInfosPane.js
+++ src/components/ArtworkInfosPane/ArtworkInfosPane.js
@@ -1,22 +1,42 @@
 import React, { Component } from "react";
+import "./ArtworkInfosPane.css";
+import "react-sliding-pane/dist/react-sliding-pane.css";
 import SlidingPane from "react-sliding-pane";
+import Modal from "react-modal";
 
 class ArtworkInfosPane extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isPaneOpen: false
+    };
+  }
+
+  componentDidMount() {
+    Modal.setAppElement(this.el);
+  }
+
   render() {
-    const { isOpen, onClose } = this.props;
-debugger
     return (
-      <SlidingPane
-        isOpen={isOpen}
-        title={"title"}
-        subtitle={"subtitle"}
-        from="right"
-        width="100%"
-        onRequestClose={onClose}
-        // className="width-100-xs width-80-sm"
-      >
-        <div>fields</div>
-      </SlidingPane>
+      <div ref={ref => (this.el = ref)}>
+        <button onClick={() => this.setState({ isPaneOpen: true })}>
+          Click me to open right pane!
+        </button>
+        <SlidingPane
+          // className="width-100-xs width-80-sm"
+          isOpen={this.state.isPaneOpen}
+          title={"title"}
+          subtitle={"subtitle"}
+          from="right"
+          width="100%"
+          onRequestClose={() => {
+            this.setState({ isPaneOpen: false });
+          }}
+        >
+          <div>And I am pane content. BTW, what rocks?</div>
+          <br />
+        </SlidingPane>
+      </div>
     );
   }
 }
