b8a0764fdb66551e7d1042e8472d8b9e250b83bf
David Huveau <d.huveau@gmail.com>
2019-08-20 15:27:04 +0200

    change the navigation component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,26 +1,13 @@
 import React from "react";
 import "./Navigation.css";
-import { Nav } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
     <div id="Nav-wrapper">
-      <Nav
-        className="justify-content-center"
-        variant="pills"
-        defaultActiveKey="/"
-        // onSelect={selectedKey => alert(`selected ${selectedKey}`)}
-      >
-        <Nav.Item bsPrefix="Nav-item">
-          {/* <Nav.Link href="/">Explore</Nav.Link> */}
-          <NavLink exact to="/">Explore</NavLink>
-        </Nav.Item>
-        <Nav.Item bsPrefix="Nav-item">
-          {/* <Nav.Link eventKey="suggest">Suggest</Nav.Link> */}
-          <NavLink to="/suggest">Suggest</NavLink>
-        </Nav.Item>
-      </Nav>
+      <Link to="/suggest" className="Nav-link btn btn-primary">
+        Suggest
+      </Link>
     </div>
   );
 };
