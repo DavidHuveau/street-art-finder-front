cc19a51faa344049c6f0e7ba29d4175e7c2e5e69
David Huveau <d.huveau@gmail.com>
2019-08-26 18:58:05 +0200

    fix the counter display

--- src/containers/ArtworksProposalsList/ArtworksProposalsList.js
+++ src/containers/ArtworksProposalsList/ArtworksProposalsList.js
@@ -1,20 +1,20 @@
-import React, { Fragment } from "react";
+import React from "react";
 import "./ArtworksProposalsList.css";
 import { CardDeck } from "react-bootstrap";
 import ArtworkProposalCard from "../../components/ArtworkProposalCard/ArtworkProposalCard";
 
 const ArtworksProposalsList = props => {
   return (
-    <Fragment>
-      <CardDeck className="proposals-list-container">
-        <small className="text-muted ml-2">333 proposal(s) to valide</small>
+    <div className="proposals-list-container">
+      <small className="text-muted ml-2">333 proposal(s) to valide</small>
+      <CardDeck>
         <ArtworkProposalCard />
         <ArtworkProposalCard />
         {/* <ArtworkProposalCard /> */}
         {/* <ArtworkProposalCard />
       <ArtworkProposalCard /> */}
       </CardDeck>
-    </Fragment>
+    </div>
   );
 };
 
