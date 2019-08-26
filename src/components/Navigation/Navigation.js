9008186ca3934ebac2eaa7405b685f39b0a38c83
David Huveau <d.huveau@gmail.com>
2019-08-26 18:35:07 +0200

    add props to set the path and caption to show in button

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./Navigation.css";
 import { Link } from "react-router-dom";
 
-const Navigation = () => {
+const Navigation = ({path, caption}) => {
   return (
     <div id="Nav-wrapper">
-      <Link to="/suggest" className="Nav-link btn btn-primary">
-        Suggest
+      <Link to={path} className="Nav-link btn btn-primary">
+        {caption}
       </Link>
     </div>
   );
--- src/containers/ArtworksExplore/ArtworksExplore.js
+++ src/containers/ArtworksExplore/ArtworksExplore.js
@@ -23,7 +23,7 @@ const ArtworksExplore = props => {
         <ArtworksMap />
       </main>
       <footer>
-        <Navigation />
+        <Navigation path="/suggest" caption="Suggest" />
       </footer>
     </div>
   );
